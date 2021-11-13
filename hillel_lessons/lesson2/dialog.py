d0cde4f99dff50c37cb3cc36d06ac57fecadf1ef
Nikolay <ppc.khizhnichenko@gmail.com>
2021-11-13 17:37:03 +0200

    First commit added README.md added .gitignore added lesson1 we lerning write "Hello World" by using print added lesson 2 README and homework

--- hillel_lessons/lesson2/dialog.py
+++ hillel_lessons/lesson2/dialog.py
@@ -1,6 +1,6 @@
 import datetime
 now = datetime.datetime.now()
 name = input("Привет меня зовут Николай, а тебя как?\n")
-age = int(input("Сколько тебе лет "+name+"?\n"))
-print("Значит ты родился в "+str(now.year - age)+" году!", end=' ')
-print("Замечательное было время! ;)")
+age = int(input("Сколько тебе лет, "+name+"?\n"))
+print("Значит ты родился в "+str(now.year - age)+" году.", end=' ')
+print("Замечательное было время!")
