d1f6dd14d36a5cc02875bb9ce2e8d0c15667a088
Nikolay <ppc.khizhnichenko@gmail.com>
2021-11-13 17:19:25 +0200

    First commit added README.md added .gitignore added lesson1 we lerning write "Hello World" by using print

---
+++ hillel_lessons/lesson2/dialog.py
@@ -0,0 +1,6 @@
+import datetime
+now = datetime.datetime.now()
+name = input("Привет меня зовут Николай, а тебя как?\n")
+age = int(input("Сколько тебе лет "+name+"?\n"))
+print("Значит ты родился в "+str(now.year - age)+" году!", end=' ')
+print("Замечательное было время! ;)")
