4960e88bdb0db439787d8972475eab819d307d33
Nikolay <ppc.khizhnichenko@gmail.com>
2021-11-26 15:29:14 +0200

    Commit added README.md added .gitignore added lesson1 we're learning to write "Hello World" by using print added lesson 2 README and homework added some fixes added text to README added gitignore text added lesson 3 added update readme lesson3 added lesson 4 task 1 - 5

--- hillel_lessons/lesson4/task_3.py
+++ hillel_lessons/lesson4/task_3.py
@@ -1,15 +1,9 @@
-num = int(input("Введите число: "))
-x1 = num % 10
-x = num // 10
+num = input("Введите число: ")
 flag = 0
-while x > 0:
-    x2 = x % 10
-    if x1 != x2:
-        x = x // 10
-        x1 = x2
-    else:
+for i in range(len(num) - 1):
+    if num[i] == num[i + 1]:
+        print("Найдены примыкающие дубли")
         flag = 1
-        print("Найден примыкающий дубль", x1)
         break
 if flag == 0:
-    print("Дублей рядом нет")
+    print("Примыкающие дубли не найдены")
