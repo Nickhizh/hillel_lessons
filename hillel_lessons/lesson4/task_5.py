55274b933a34f9673116cece0bcb8fea18980344
Nikolay <ppc.khizhnichenko@gmail.com>
2021-11-26 14:32:38 +0200

    Commit added README.md added .gitignore added lesson1 we're learning to write "Hello World" by using print added lesson 2 README and homework added some fixes added text to README added gitignore text added lesson 3 added update readme lesson3 added lesson 4 task 1 - 5

--- hillel_lessons/lesson4/task_5.py
+++ hillel_lessons/lesson4/task_5.py
@@ -1,13 +1,10 @@
-num1 = list(input("Введите число: "))
-num2 = [int(x) for x in num1]
+s = input("Ввод: ")
+ch = input("Какой символ найти? Ввод: ")
 
-trigger = 0
-for x in range(0, 10):
-    y = num2.count(x)
-    if y > 1:
-        print("Найден дубль")
-        trigger = 1
+i = s.find(ch)
+
+for x in s:
+    print("индекс символа: ", i)
+    i = s.find(ch, i + 1)
+    if i == -1:
         break
-
-if trigger == 0:
-    print("Дубль не найден")
