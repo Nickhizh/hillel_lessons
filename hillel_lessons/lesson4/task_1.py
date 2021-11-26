3f3771af9b88f26955e024a798eb7c5c4432bc95
Nikolay <94188708+Nickhizh@users.noreply.github.com>
2021-11-26 15:34:27 +0200

    Update task_1.py

--- hillel_lessons/lesson4/task_1.py
+++ hillel_lessons/lesson4/task_1.py
@@ -1,13 +1,13 @@
 num1 = list(input("Введите число: "))
 num2 = [int(x) for x in num1]
 
-trigger = 0
+flag = 0
 for x in range(0, 10):
     y = num2.count(x)
     if y > 1:
         print("Найден дубль")
-        trigger = 1
+        flag = 1
         break
 
-if trigger == 0:
+if flag == 0:
     print("Дубль не найден")
