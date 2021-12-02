0c0d151d46a900e444f0172e62ff837cbdb76b04
Nikolay <ppc.khizhnichenko@gmail.com>
2021-12-02 12:33:21 +0200

    Commit added README.md added .gitignore added lesson1 we're learning to write "Hello World" by using print added lesson 2 README and homework added some fixes added text to README added gitignore text added lesson 3 added update readme lesson3 added lesson 4 task 1 - 5 added lesson 5 fixed issue on lesson 4 added lesson 6

--- hillel_lessons/lesson6/task_3.py
+++ hillel_lessons/lesson6/task_3.py
@@ -1,15 +1,26 @@
 import random
+# Способ решения путем вывода общего количества уникальных значений
+lst_1 = [random.randint(0, 10) for x in range(5)]
+lst_2 = [random.randint(0, 10) for y in range(5)]
 
+print(f" {len([x for x in lst_1 + lst_2 if (lst_1 + lst_2).count(x) == 1])} уникальных значений")
+
+# Способ решения путем вывода  количества уникальных значений для каждого списка
 # lst_1 = [random.randint(0, 10) for x in range(5)]
 # lst_2 = [random.randint(0, 10) for y in range(5)]
-# print(lst_1)
-# print(lst_2)
-# print([z for z in lst_1 if z not in lst_2])
-# print(len([z for z in lst_1 if z not in lst_2]))
 
-# print(list(set(lst_1)))
+# print(f"В первом списке {len([x for x in lst_1 if lst_1.count(x) == 1 and x not in lst_2])} уникальных значений, "
+#       f"а во втором {len([x for x in lst_2 if lst_2.count(x) == 1 and x not in lst_1])}"
+#       )
 
-lst_1 = [1, 3, 3]
-lst_2 = [1, 5, 4]
-print(f"В lst_1 {len([z for z in lst_1 if z not in lst_2])} уникальных цифры, а в lst_2 {len([z for z in lst_2 if z not in lst_1])}")
+
+
+
+
+
+
+
+
+
+
 
