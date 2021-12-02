82e34d7caa7f196fe44af7c8d76eb7886952f815
Nikolay <ppc.khizhnichenko@gmail.com>
2021-12-02 09:19:59 +0200

    Commit added README.md added .gitignore added lesson1 we're learning to write "Hello World" by using print added lesson 2 README and homework added some fixes added text to README added gitignore text added lesson 3 added update readme lesson3 added lesson 4 task 1 - 5 added lesson 5 fixed issue on lesson 4

--- hillel_lessons/lesson4/task_2.py
+++ hillel_lessons/lesson4/task_2.py
@@ -3,4 +3,4 @@ for i in range(1, num):
     a = str(i)
     b = str(i ** 2)
     if a == b[-len(a):] and int(a) <= num:
-        print(a + " * " + a + " = " + b)
+        print(a + "*" + a + "=" + b)
