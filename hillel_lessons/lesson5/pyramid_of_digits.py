4f554fc8c64093f8298a161fe32c08d4ad9e485f
Nickhizh <ppc.khizhnichenko@gmail.com>
2021-12-04 10:44:40 +0200

    add lesson 4 completed

--- hillel_lessons/lesson5/pyramid_of_digits.py
+++ hillel_lessons/lesson5/pyramid_of_digits.py
@@ -1,14 +1,20 @@
-user_input = input("Input number from 3 to 9: ")                 # Ввод пользователя
+# Ввод пользователя
+user_input = input("Input number from 3 to 9: ")
+# Проверка на верность ввода
+if user_input.isdigit() and int(user_input) in range(3, 10):
 
-if user_input.isdigit() and int(user_input) in range(3, 10):     # Проверка на верность ввода
-    for x in range(1, int(user_input) + 1):                      # Перебор значений от 1 до ввода пользователя
-        for y in range(1, x + 1):                                # Вывод чисел в выбранном диапазоне
+    # Перебор значений от 1 до ввода пользователя
+    for x in range(1, int(user_input) + 1):
+
+        # Вывод чисел в выбранном диапазоне
+        for y in range(1, x + 1):
             print(y, end="")
-        for y in range(x - 1, 0, -1):                            # Зеркальный вывод тех же чисел в том же ряду
+        # Зеркальный вывод тех же чисел в том же ряду
+        for y in range(x - 1, 0, -1):
             print(y, end="")
-        print()                                                  # Для того чтобы след. итерация была в новой строке
+        # Для того чтобы след. итерация была в новой строке
+        print()
+
+# Сообщение при не верном вводе
 else:
-    print("Wrong input. Use only number in range 3 to 9")        # Сообщение при не верном вводе
-
-
-
+    print("Wrong input. Use only number in range 3 to 9")
