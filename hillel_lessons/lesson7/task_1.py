c8d37ca3bb8949e874a82fe0163ca330a6c2c808
Nickhizh <ppc.khizhnichenko@gmail.com>
2021-12-12 18:18:05 +0200

    add lesson 4 completed

--- hillel_lessons/lesson7/task_1.py
+++ hillel_lessons/lesson7/task_1.py
@@ -1,3 +1,14 @@
 import random
+# случайный список целый чисел
+lst = [random.randint(0, 10) for x in range(10)]
 
-lst = [random.randint(0, 10) for x in range(5)]
+# Для наглядности вывод списка начального
+print(lst)
+
+# Вывод нового списка из уникальных чисел
+# реверс списка и превращение его в кортеж
+print(tuple([x for x in lst if lst.count(x) == 1][::-1]))
+
+
+
+
