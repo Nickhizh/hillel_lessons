ea7e9fb66ca2e3714ec78c60e1762efab8783bb0
Nickhizh <ppc.khizhnichenko@gmail.com>
2021-12-12 21:44:54 +0200

    add lesson 4 completed add lesson 7 completed

--- hillel_lessons/lesson7/task_4.py
+++ hillel_lessons/lesson7/task_4.py
@@ -3,14 +3,17 @@ import re
 
 some_string = re.sub(r'[^\w\s]', "", input(" Введите текст: \n"))
 some_dict = {}
+
 for key in some_string.split():
 
     # Перевожу все ключи в нижний регистр и назначаю ключам значения повторений
     key = key.lower()
     some_dict[key] = some_dict.setdefault(key, 0) + 1
 
+# нахожу максимальное значение повторений
+max_value = 0
 for key, value in some_dict.items():
-    print(f'Слово "{key}" встречается {value} раз')
-
-
-
+    if max_value <= value:
+        max_value = value
+        result = key
+print(result)
