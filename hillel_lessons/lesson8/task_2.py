ba310b4d63411171bf762111290969caa169c17d
Nickhizh <ppc.khizhnichenko@gmail.com>
2021-12-12 22:51:40 +0200

    add lesson 4 completed add lesson 7 completed add lesson 8 completed

--- hillel_lessons/lesson8/task_2.py
+++ hillel_lessons/lesson8/task_2.py
@@ -2,4 +2,4 @@ import random
 
 lst_1 = [random.randint(0, 50) for i in range(10)]
 lst_2 = [random.randint(0, 50) for i in range(10)]
-print(len(set([x for x in lst_1 if x in lst_2])))
+print(f"Разных чисел: {len(set([x for x in lst_1 if x in lst_2]))}")
