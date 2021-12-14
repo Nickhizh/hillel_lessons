8edb768c5c9466132696def2a325865dd97ac2d7
Nickhizh <ppc.khizhnichenko@gmail.com>
2021-12-14 15:06:33 +0200

    add lesson 4 completed add lesson 7 completed

--- hillel_lessons/lesson9/task_1.py
+++ hillel_lessons/lesson9/task_1.py
@@ -1,4 +1,15 @@
-nums = [1, 2, 55, 5, 5, 17, 9, 0]
-num = 10
+def findsum():
+    nums = [int(x) for x in input("Введите числа: ").split()]
+    dgt = int(input("Введите число: "))
+    size = len(nums)
 
-for x in nums:
+    for i in range(0, size - 1):
+        for j in range(i + 1, size):
+            if nums[i] + nums[j] == dgt:
+                print(f"Пары для {dgt} найдены: {nums[i]} и {nums[j]}")
+                return True
+    print(f"Пары для {dgt} не найдены")
+    return False
+
+
+findsum()
