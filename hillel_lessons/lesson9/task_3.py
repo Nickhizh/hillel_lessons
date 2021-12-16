b570066b42be5fc8af8dfe0c90e251bf75d9de5b
Nickhizh <ppc.khizhnichenko@gmail.com>
2021-12-16 22:17:00 +0200

    add lesson 4 completed add lesson 7 completed

--- hillel_lessons/lesson9/task_3.py
+++ hillel_lessons/lesson9/task_3.py
@@ -4,14 +4,14 @@ print([x for x in range(2, 101) if not [n for n in range(2, x) if not x % n]])
 # Вариант функции
 def findprime():
     for i in range(1, 101):
-        deli = []
+        primes = []
         for a in range(2, i+1):
             if i % a == 0:
-                deli.append(a)
-                if len(deli) > 1:
+                primes.append(a)
+                if len(primes) > 1:
                     break
-        if len(deli) == 1:
-            print(*deli, sep=", ", end=' ')
+        if len(primes) == 1:
+            print(*primes, sep=", ", end=' ')
 
 
 findprime()
