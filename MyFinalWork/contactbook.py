7dd21f924e55b1d245b504d1d5a095918c8a7599
Nikolay <ppc.khizhnichenko@gmail.com>
2022-01-19 15:16:02 +0200

    First commit

---
+++ MyFinalWork/contactbook.py
@@ -0,0 +1,125 @@
+import pickle
+import os
+
+UI = '''
+1. Добавить контакт
+2. Все контакты
+3. Поиск
+4. Изменить контакт
+5. Удалить контакт
+6. Удалить все
+7. Сохранить и выйти
+'''
+
+
+class Person(object):
+
+    def __init__(self, name=None, surname=None, address=None, phone=None, birthday=None):
+        self.name = name
+        self.surname = surname
+        self.address = address
+        self.phone = phone
+        self.birthday = birthday
+
+    def __str__(self):
+        return "{} {:>15} {:>14} {:>15} {:>15}".format(self.name, self.surname, self.address, self.phone, self.birthday)
+
+
+class Application(object):
+
+    def __init__(self, database):
+        self.database = database
+        self.persons = {}
+        if not os.path.exists(self.database):
+            file_pointer = open(self.database, 'wb')
+            pickle.dump({}, file_pointer)
+            file_pointer.close()
+        else:
+            with open(self.database, 'rb') as person_list:
+                self.persons = pickle.load(person_list)
+
+    def add(self):
+        name, surname, address, phone, birthday = self.getdetails()
+        if name not in self.persons:
+            self.persons[name] = Person(name, surname, address, phone, birthday)
+        else:
+            print("Такой контакт уже существует")
+
+    def viewall(self):
+        if self.persons:
+            print("{:>5} {:>15} {:>15} {:>15} {:>18}".format('Имя', 'Фамилия', 'Адрес', 'Телефон', 'Дата рождения'))
+            for person in self.persons.values():
+                print(person)
+        else:
+            print("Записная книжка пуста")
+
+    def search(self):
+        name = input("Введите имя: ")
+        if name in self.persons:
+            print(self.persons[name])
+        else:
+            print("Контакт не найден")
+
+    def getdetails(self):
+        name = input("Имя: ")
+        surname = input("Фамилия: ")
+        address = input("Адрес: ")
+        phone = input("Номер телефона:")
+        birthday = input("Дата рождения:")
+        return name, surname, address, phone, birthday
+
+    def update(self):
+        name = input("Введите имя: ")
+        if name in self.persons:
+            print("Найдено. Продолжить изменения.")
+            name, surname, address, phone, birthday = self.getdetails()
+            self.persons[name].__init__(name, surname, address, phone, birthday)
+            print("Изменено")
+        else:
+            print("Контакт не найден")
+
+    def delete(self):
+        name = input("Введите Имя для удаления: ")
+        if name in self.persons:
+            del self.persons[name]
+            print("Контакт удален")
+        else:
+            print("Контакт не найден")
+
+    def reset(self):
+        self.persons = {}
+
+    def __del__(self):
+        with open(self.database, 'wb') as db:
+            pickle.dump(self.persons, db)
+
+    def __str__(self):
+        return UI
+
+
+def main():
+    app = Application('mycontacts.data')
+    choice = ''
+    while choice != '7':
+        print(app)
+        choice = input('Введите цифру: ')
+        if choice == '1':
+            app.add()
+        elif choice == '2':
+            app.viewall()
+        elif choice == '3':
+            app.search()
+        elif choice == '4':
+            app.update()
+        elif choice == '5':
+            app.delete()
+        elif choice == '6':
+            app.reset()
+        elif choice == '7':
+            print("Сохранено")
+        else:
+            print("Неверный ввод.")
+
+
+if __name__ == '__main__':
+    main()
